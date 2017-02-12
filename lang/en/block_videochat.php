1d4cedc3b0100a683426d796a9ccb5a67cc6b635
Kien Vu <kien@brightcookie.com.au>
2017-02-12 23:56:10 +1030

    Update README.md and add all files to commit

---
+++ lang/en/block_videochat.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * videochat block.
+ *
+ * @copyright 2016 Kien Vu <vuthekien@gmail.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['pluginname'] = 'Video chat';
+$string['videochat'] = 'Video chat';
+$string['videochat:addinstance'] = 'Add a new video Chat block';
+$string['videochat:myaddinstance'] = 'Add a new video Chat block to the My Moodle page';
+$string['conferenceurl'] = 'Video conference URL';
+$string['conferenceurldesc'] = 'Please choose a video conference provider';
+$string['defaultactivetime'] = 'User active time';
+$string['defaultactivetimedesc'] = 'User active time in second to check if user is on-line/active';
+$string['activeuser'] = 'Active users';
+$string['currentbusy'] = 'User {$a} is currently busy';
+$string['invaliduser'] = 'Invalid call to user';
+$string['invalidcall'] = 'This call is no longer valid';
+$string['incall'] = 'In call';
+$string['comingcall'] = 'Coming call';
+$string['outgoingcall'] = 'Outgoing call';
+$string['youcurrentbusy'] = 'You need to hangup before making another call';
